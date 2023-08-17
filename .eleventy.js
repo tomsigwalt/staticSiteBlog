6a286a58e58795f6d4de5364716fc75841c160f1
Thomas Sigwalt <tomsigwalt@gmail.com>
2023-08-16 19:00:27 -0500

    this commit is for the eleventy blog I am making

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter("postDate", (dateObj)=> {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+
+   /* (automatically updating the year)  
+   eleventyConfig.addShortcode("year", () => ${new Date().getFullYear()} ); */
+
+    return {
+        dir: {
+            input:"src",
+            output:"public"
+        }
+    };
+}
---
+++ src/blog/2021-06-16-a-new-article.md
@@ -0,0 +1,14 @@
+---
+title: a new article
+description: hello world
+---
+
+wordssdlfdkfjlsfjlksjdfljas;lfdj;kalsjf;lja
+asldf;sdjflkfjlsjdf;lfjlsadfjfkfirioqperioqjfodsj
+dksjaf;ljfdsjkfsjd
+jdjkfjkjfieriwqoj
+111111
+333333
+
+222222
+sladflkasjd;fajsflkjl489rjhigv
